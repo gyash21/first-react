d43849eda8f1e5127c3a0e19b35926e3fdb95086
Yash Gupta <gyash21@gmail.com>
2022-05-02 11:04:02 +0530

    started react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <section className="booklist">
+      <Book />
+    </section >
+  );
+}
+
+const author = 'Morgan Housel'
+const Book = () => {
+  const title = 'The Psychology of Money'
+  return (
+    < article className="book" >
+      <img src='https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY327_FMwebp_QL65_.jpg' alt='' />
+      <h4>{title}</h4>
+      <h5>{author.toUpperCase()}</h5>
+    </article >);
+}
+
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
